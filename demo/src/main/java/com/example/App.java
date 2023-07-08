2b75ecda5dcc4206d56cdd1e60b31f798a94924a
AlexandreMouraa <alexandree.moouraa6@gmail.com>
2023-07-08 19:52:49 -0300

    Instalação das Bibliotecas necessárias

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package demo.src.main.java.com.example;
+
+public class App 
+{
+    private App() 
+    {
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
